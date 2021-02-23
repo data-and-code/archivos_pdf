b170dc23c68c2a9751e844d6b5bce793c650b663
Fernando Gomez Perera <160300129@ucaribe.edu.mx>
2021-02-23 01:06:13 -0500

    Fix web scraping for project 2

--- base_de_datos_2_web_scraping.R
+++ base_de_datos_2_web_scraping.R
@@ -1,5 +1,8 @@
 # Extraccion del texto de los PDFs
-pacman::p_load(pdftools, tesseract, rvest, purrr, stringr)
+pacman::p_load(pdftools, tesseract, rvest, purrr, furrr, stringr)
+
+# Definir el numero de workers que permitiran ejecutar funciones de purrr de forma paralela
+plan(multisession, workers = 4)
 
 # Obtener las direcciones de los PDFs del mes (Septiembre)
 pdf_urls <- read_html("https://www.gob.mx/salud/documentos/coronavirus-covid-19-comunicados-tecnicos-diarios-septiembre-2020") %>%
@@ -14,14 +17,82 @@ pdf_urls <- read_html("https://www.gob.mx/salud/documentos/coronavirus-covid-19-
   # Completar cada URL
   map_chr(~ str_c("https://www.gob.mx", .x))
 
-# Extraer el texto de cada PDF
-pdf_files <- pdf_urls %>%
+# Extraer el texto de cada PDF utilizando distintos valores de dpi
+pdf_files_1000 <- pdf_urls %>%
   # Usar Tesseract para detectar el texto dentro de cada PDF (incluso del texto dentro de las imagenes)
-  map(pdf_ocr_text, language = "spa") %>%
+  future_map(pdf_ocr_text, pages = c(1, 5), language = "spa", dpi = 1000) %>%
   # Renombrar cada elemento del resultado con su nombre de archivo original
   set_names(nm = str_extract(pdf_urls, pattern = "(?<=/)Comunicado.*\\.pdf$")) %>%
   # Invertir el orden del resultado para que los documentos se ordenen por fecha de forma ascendente
   rev()
+
+pdf_files_800 <- pdf_urls %>%
+  # Usar Tesseract para detectar el texto dentro de cada PDF (incluso del texto dentro de las imagenes)
+  future_map(pdf_ocr_text, pages = c(1, 5), language = "spa", dpi = 800) %>%
+  # Renombrar cada elemento del resultado con su nombre de archivo original
+  set_names(nm = str_extract(pdf_urls, pattern = "(?<=/)Comunicado.*\\.pdf$")) %>%
+  # Invertir el orden del resultado para que los documentos se ordenen por fecha de forma ascendente
+  rev()
+
+pdf_files_750 <- pdf_urls %>%
+  # Usar Tesseract para detectar el texto dentro de cada PDF (incluso del texto dentro de las imagenes)
+  future_map(pdf_ocr_text, pages = c(1, 5), language = "spa", dpi = 750) %>%
+  # Renombrar cada elemento del resultado con su nombre de archivo original
+  set_names(nm = str_extract(pdf_urls, pattern = "(?<=/)Comunicado.*\\.pdf$")) %>%
+  # Invertir el orden del resultado para que los documentos se ordenen por fecha de forma ascendente
+  rev()
+
+pdf_files_700 <- pdf_urls %>%
+  # Usar Tesseract para detectar el texto dentro de cada PDF (incluso del texto dentro de las imagenes)
+  future_map(pdf_ocr_text, pages = c(1, 5), language = "spa", dpi = 700) %>%
+  # Renombrar cada elemento del resultado con su nombre de archivo original
+  set_names(nm = str_extract(pdf_urls, pattern = "(?<=/)Comunicado.*\\.pdf$")) %>%
+  # Invertir el orden del resultado para que los documentos se ordenen por fecha de forma ascendente
+  rev()
+
+pdf_files_650 <- pdf_urls %>%
+  # Usar Tesseract para detectar el texto dentro de cada PDF (incluso del texto dentro de las imagenes)
+  future_map(pdf_ocr_text, pages = c(1, 5), language = "spa", dpi = 650) %>%
+  # Renombrar cada elemento del resultado con su nombre de archivo original
+  set_names(nm = str_extract(pdf_urls, pattern = "(?<=/)Comunicado.*\\.pdf$")) %>%
+  # Invertir el orden del resultado para que los documentos se ordenen por fecha de forma ascendente
+  rev()
+
+pdf_files_600 <- pdf_urls %>%
+  # Usar Tesseract para detectar el texto dentro de cada PDF (incluso del texto dentro de las imagenes)
+  future_map(pdf_ocr_text, pages = c(1, 5), language = "spa", dpi = 600) %>%
+  # Renombrar cada elemento del resultado con su nombre de archivo original
+  set_names(nm = str_extract(pdf_urls, pattern = "(?<=/)Comunicado.*\\.pdf$")) %>%
+  # Invertir el orden del resultado para que los documentos se ordenen por fecha de forma ascendente
+  rev()
+
+pdf_files_550 <- pdf_urls %>%
+  # Usar Tesseract para detectar el texto dentro de cada PDF (incluso del texto dentro de las imagenes)
+  future_map(pdf_ocr_text, pages = c(1, 5), language = "spa", dpi = 550) %>%
+  # Renombrar cada elemento del resultado con su nombre de archivo original
+  set_names(nm = str_extract(pdf_urls, pattern = "(?<=/)Comunicado.*\\.pdf$")) %>%
+  # Invertir el orden del resultado para que los documentos se ordenen por fecha de forma ascendente
+  rev()
+
+pdf_files_500 <- pdf_urls %>%
+  # Usar Tesseract para detectar el texto dentro de cada PDF (incluso del texto dentro de las imagenes)
+  future_map(pdf_ocr_text, pages = c(1, 5), language = "spa", dpi = 500) %>%
+  # Renombrar cada elemento del resultado con su nombre de archivo original
+  set_names(nm = str_extract(pdf_urls, pattern = "(?<=/)Comunicado.*\\.pdf$")) %>%
+  # Invertir el orden del resultado para que los documentos se ordenen por fecha de forma ascendente
+  rev()
+
+# Almacenar los resultados en una sola lista
+pdf_files <- list(
+  dpi_500 = pdf_files_500,
+  dpi_550 = pdf_files_550,
+  dpi_600 = pdf_files_600,
+  dpi_650 = pdf_files_650,
+  dpi_700 = pdf_files_700,
+  dpi_750 = pdf_files_750,
+  dpi_800 = pdf_files_800,
+  dpi_1000 = pdf_files_1000
+)
 
 # Se almacena el resultado en un archivo RData para usarlo el resto del proyecto
 save(pdf_files, file = "base_de_datos_2.RData")
